69a622f8c9968d09c9fef3dbff59e8c0326f67a9
NL-User <25559838+NL-User@users.noreply.github.com>
2020-02-08 18:29:46 +0900

    合法手を取得する関数を実装

--- BitBoard-Othello.cpp
+++ BitBoard-Othello.cpp
@@ -1,15 +1,16 @@
 ﻿#include "pch.h"
 #include <stdio.h>
 #include <chrono>
+#include <fstream>
 
 using namespace std;
+using ull_int = unsigned long long int;
 
 const int kBoardSize = 8; // 何×何か
 const int kCellsCount = kBoardSize * kBoardSize;
 const bool kIsBackgroundBlack = true;
 
-
-int Count_Bits(unsigned long long int bits) {
+int Count_Bits(ull_int bits) {
 	bits = (bits & 0x5555555555555555) + (bits >> 1 & 0x5555555555555555);
 	bits = (bits & 0x3333333333333333) + (bits >> 2 & 0x3333333333333333);
 	bits = (bits & 0x0f0f0f0f0f0f0f0f) + (bits >> 4 & 0x0f0f0f0f0f0f0f0f);
@@ -21,17 +22,19 @@ int Count_Bits(unsigned long long int bits) {
 
 class Board {
 public:
-	Board(unsigned long long int, unsigned long long int, bool);
+	Board(ull_int, ull_int, bool);
 	~Board();
 	void View();
 	int Get_Board_Piece_Count();
 
 private:
-	unsigned long long int black_board, white_board;
+	ull_int Get_Legal_Move();
+
+	ull_int black_board, white_board;
 	bool is_black_turn;
 };
 
-Board::Board(unsigned long long int black_ = 0, unsigned long long int white_ = 0, bool turn_ = true) {
+Board::Board(ull_int black_ = 0, ull_int white_ = 0, bool turn_ = true) {
 	if (black_ == 0 || white_ == 0) {
 		black_board = 0x0000000810000000;
 		white_board = 0x0000001008000000;
@@ -47,6 +50,8 @@ Board::~Board() {
 }
 
 void Board::View() {
+	ull_int ligal_move = Get_Legal_Move();
+
 	printf("%sのターン\n%dターン目\n\n  ", is_black_turn ? "黒" : "白", Get_Board_Piece_Count() - 3);
 	for (int i = 97; i < 105; i++) {
 		printf("  %c", i);
@@ -60,6 +65,8 @@ void Board::View() {
 				printf((kIsBackgroundBlack) ? "〇" : "●");
 			} else if (white_board >> (kBoardSize*i - j) & 0x1) {
 				printf((kIsBackgroundBlack) ? "●" : "〇");
+			} else if (ligal_move >> (kBoardSize*i - j) & 0x1) {
+				printf("△");
 			} else {
 				printf("－");
 			}
@@ -73,9 +80,58 @@ int Board::Get_Board_Piece_Count() {
 	return Count_Bits(black_board) + Count_Bits(white_board);
 }
 
+ull_int Board::Get_Legal_Move() {
+	ull_int player, opponent;
+	if (is_black_turn) {
+		player = black_board;
+		opponent = white_board;
+	} else {
+		player = black_board;
+		opponent = white_board;
+	}
+
+	ull_int blank = ~(player | opponent);
+	
+	ull_int ligal_move = 0;
+	ull_int tmp, mask;
+
+	for (int i = 0; i < 2; i++) {
+		for (int d : {1, 7, 8, 9}) {
+			if (d == 1) {
+				// 横
+				mask = opponent & 0x7e7e7e7e7e7e7e7e;
+			} else if (d == 8) {
+				// 縦
+				mask = opponent & 0x00ffffffffffff00;
+			} else {
+				// 斜め
+				mask = opponent & 0x007e7e7e7e7e7e00;
+			}
+
+			if (i == 0) {
+				tmp = mask & (player << d);
+				tmp |= mask & (tmp << d);
+				tmp |= mask & (tmp << d);
+				tmp |= mask & (tmp << d);
+				tmp |= mask & (tmp << d);
+				tmp |= mask & (tmp << d);
+				ligal_move |= blank & (tmp << d);
+			} else {
+				tmp = mask & (player >> d);
+				tmp |= mask & (tmp >> d);
+				tmp |= mask & (tmp >> d);
+				tmp |= mask & (tmp >> d);
+				tmp |= mask & (tmp >> d);
+				tmp |= mask & (tmp >> d);
+				ligal_move |= blank & (tmp >> d);
+			}
+		}
+	}
+
+	return ligal_move;
+}
 
 int main() {
-
 	chrono::system_clock::time_point start_time, end_time;
 
 	start_time = chrono::system_clock::now();
@@ -86,5 +142,8 @@ int main() {
 	end_time = chrono::system_clock::now();
 
 	printf("処理時間:%dミリ秒", (int)chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count());
+
+	ofstream file("ull-int.txt", ios::app);
+	file << (int)chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count() << ",";
 	_getwch();
 }
